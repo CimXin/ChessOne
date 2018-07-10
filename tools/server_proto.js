9cdafcc52bb71318cb73b0cd97cfd91f38b65d22
CimXin <372134690@qq.com>
2018-07-10 16:25:31 +0800

    server-protobuf generate script is ok!

--- tools/server_proto.js
+++ tools/server_proto.js
@@ -1,13 +1,42 @@
-console.log("================================\n启动服务端脚本 \n ->将多协议文件合成一个json文件");
+console.log("================================\n 启动服务端脚本 \n  ->将多协议文件合成一个json文件");
 
-var config = require('./config');
-var rootPath = config.rootPath();
-var protoPath = config.protoPath();
-var serverProtoPath = config.serverProtoPath();
+var fs = require("fs");
 
-var exec = require('child_process').exec;
-var cmd = "pbjs -t json test.proto test1.proto  > test.json";
-cmd = `pbjs -t json ${protoPath+"/GamePlayer.proto"}  > ${serverProtoPath+"/proto.json"}`
-exec(cmd, function (error, stdout, stderr) {
-    console.log(error, stdout, stderr);
-});
+function main() {
+    var config = require('./config');
+    var protoPath = config.protoPath();
+    var serverProtoPath = config.serverProtoPath();
+    var protoFiles = travelProtoFiles(protoPath);
+
+    checkServerProtoPath(serverProtoPath);
+    var exec = require('child_process').exec;
+    var cmd = "pbjs -t json test.proto test1.proto  > test.json";
+    cmd = `pbjs -t json ${protoFiles}  > ${serverProtoPath+"/proto.json"}`
+    exec(cmd, function (error, stdout, stderr) {
+        console.log(" 合成proto.json文件完毕！\n================================")
+    });
+}
+
+/**
+ * 遍历proto文件夹所有proto文件
+ */
+function travelProtoFiles(protoPath) {
+    var fileNames = fs.readdirSync(protoPath);
+    var allFileNames = "";
+    fileNames.forEach((name) => {
+        console.log("  -->", name);
+        allFileNames += protoPath + "/" + name + " ";
+    });
+    return allFileNames;
+}
+
+/**
+ * 检查服务端是否存在message文件夹
+ */
+function checkServerProtoPath(protoPath) {
+    if (!fs.existsSync(protoPath)) {
+        fs.mkdirSync(protoPath);
+    }
+}
+
+main();
